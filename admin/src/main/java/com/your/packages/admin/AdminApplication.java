58e93f500e29b5516ebd2e07ff018f01c1765e1a
Hccake <chengbohua@foxmail.com>
2023-04-24 21:56:31 +0800

    :alien: 同步 ballcat v1.1.0 版本改动

--- admin/src/main/java/com/your/packages/admin/AdminApplication.java
+++ admin/src/main/java/com/your/packages/admin/AdminApplication.java
@@ -1,5 +1,7 @@
 package com.your.packages.admin;
 
+import org.ballcat.springsecurity.oauth2.server.authorization.annotation.EnableOauth2AuthorizationServer;
+import org.ballcat.springsecurity.oauth2.server.resource.annotation.EnableOauth2ResourceServer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author Hccake
  */
+@EnableOauth2AuthorizationServer
+@EnableOauth2ResourceServer
 @MapperScan({ "com.your.packages.**.mapper" })
 @SpringBootApplication
 public class AdminApplication {
---
+++ admin/src/test/java/com/your/packages/admin/OAuth2RegisteredClientTest.java
@@ -0,0 +1,105 @@
+package com.your.packages.admin;
+
+import org.ballcat.springsecurity.oauth2.server.authorization.autoconfigure.OAuth2AuthorizationServerAutoConfiguration;
+import org.ballcat.springsecurity.oauth2.server.resource.ResourceServerAutoConfiguration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.test.annotation.Rollback;
+
+import java.time.Duration;
+import java.util.UUID;
+
+/**
+ * @author hccake
+ */
+@JdbcTest(excludeAutoConfiguration = { OAuth2AuthorizationServerAutoConfiguration.class,
+		ResourceServerAutoConfiguration.class })
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class OAuth2RegisteredClientTest {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Test
+	@Rollback(false)
+	void createUiClient() {
+		JdbcRegisteredClientRepository jdbcRegisteredClientRepository = new JdbcRegisteredClientRepository(
+				jdbcTemplate);
+
+		String clientId = "ui";
+		String clientSecret = "{noop}ui";
+
+		RegisteredClient client = jdbcRegisteredClientRepository.findByClientId(clientId);
+		if (client == null) {
+			RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+				.clientId(clientId)
+				.clientSecret(clientSecret)
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+				.authorizationGrantType(AuthorizationGrantType.PASSWORD)
+				.redirectUri("http://127.0.0.1:8080/authorized")
+				// .scope("skip_captcha") // 跳过验证码
+				// .scope("skip_password_decode") // 跳过 AES 密码解密
+				.tokenSettings(TokenSettings.builder()
+					// 使用不透明令牌
+					.accessTokenFormat(OAuth2TokenFormat.REFERENCE)
+					.accessTokenTimeToLive(Duration.ofDays(1))
+					.refreshTokenTimeToLive(Duration.ofDays(3))
+					.build())
+				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+				.build();
+			jdbcRegisteredClientRepository.save(registeredClient);
+
+			client = jdbcRegisteredClientRepository.findByClientId(clientId);
+			Assertions.assertNotNull(client);
+		}
+	}
+
+	@Test
+	@Rollback(false)
+	void createTestClient() {
+		JdbcRegisteredClientRepository jdbcRegisteredClientRepository = new JdbcRegisteredClientRepository(
+				jdbcTemplate);
+
+		RegisteredClient client = jdbcRegisteredClientRepository.findByClientId("test");
+		if (client == null) {
+			RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+				.clientId("test")
+				.clientSecret("{noop}test")
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+				.authorizationGrantType(AuthorizationGrantType.PASSWORD)
+				.redirectUri("http://127.0.0.1:8080/authorized")
+				.scope("skip_captcha") // 跳过验证码
+				.scope("skip_password_decode") // 跳过 AES 密码解密
+				.tokenSettings(TokenSettings.builder()
+					// 使用不透明令牌
+					.accessTokenFormat(OAuth2TokenFormat.REFERENCE)
+					.accessTokenTimeToLive(Duration.ofDays(1))
+					.refreshTokenTimeToLive(Duration.ofDays(3))
+					.build())
+				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+				.build();
+			jdbcRegisteredClientRepository.save(registeredClient);
+
+			client = jdbcRegisteredClientRepository.findByClientId("test");
+			Assertions.assertNotNull(client);
+		}
+	}
+
+}
